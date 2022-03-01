1a596d61e0fc5deffefabc4d308221f54cdf585a
Miso Menze <72578071+Miso-0@users.noreply.github.com>
2022-03-01 16:52:33 +0200

    m

--- quickorder/lib/ui/utils/reusable_widgets.dart
+++ quickorder/lib/ui/utils/reusable_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boxicons/flutter_boxicons.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
